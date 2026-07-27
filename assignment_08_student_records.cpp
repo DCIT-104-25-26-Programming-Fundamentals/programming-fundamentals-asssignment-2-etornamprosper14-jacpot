fa84199288e6b66848ec72f7d010919e54388912
Etornam Prosper <etornamprosper14@gmail.com>
2026-07-27 13:41:47 +0100

    complete assignment 8 student records

--- assignment_08_student_records.cpp
+++ assignment_08_student_records.cpp
@@ -83,3 +83,46 @@
 #include <iomanip>
 using namespace std;
 
+// PART A - Single Table
+void printTable(int num) {
+    cout << "Multiplication Table for " << num << ":" << endl;
+    for (int i = 1; i <= 12; i++) {
+        cout << num << " x " << i << " = " << (num * i) << endl;
+    }
+}
+
+// PART B - Bonus: Tables from 1 to N
+void printTablesUpToN(int n) {
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return;
+    }
+
+    for (int num = 1; num <= n; num++) {
+        printTable(num);
+        if (num != n) {
+            cout << "---------------------------" << endl;
+        }
+    }
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number <= 0) {
+        cout << "Error: please enter a positive integer." << endl;
+        return 1;
+    }
+
+    printTable(number);
+
+    // PART B (bonus)
+    int n;
+    cout << "\nEnter N to print tables from 1 to N: ";
+    cin >> n;
+    printTablesUpToN(n);
+
+    return 0;
+}
